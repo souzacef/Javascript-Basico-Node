e022122c506005d4fb23029a2a0e9dda4fe175f1
Carlos <souzacef@gmail.com>
2024-07-03 11:53:22 -0300

    Added multiply and divide

--- Aula 05/script.js
+++ Aula 05/script.js
@@ -1,5 +1,5 @@
 window.onload = function(){
-    document.getElementById("calcular").addEventListener("click", function(){
+    document.getElementById("num2").addEventListener("focusout", function(){
         num1 = document.getElementById("num1").value;
         num2 = document.getElementById("num2").value;
         
@@ -12,5 +12,27 @@ window.onload = function(){
         num2 = document.getElementById("num4").value;
 
         document.getElementById("result2").value = parseInt(num1) - parseInt(num2);
+    })
+
+    document.getElementById("num6").addEventListener("focusout", function(){
+        num1 = document.getElementById("num5").value;
+        num2 = document.getElementById("num6").value;
+
+        document.getElementById("result3").value = parseInt(num1) * parseInt(num2);
+    })
+
+    document.getElementById("num8").addEventListener("focusout", function(){
+        num1 = document.getElementById("num7").value;
+        num2 = document.getElementById("num8").value;
+
+        document.getElementById("result4").value = parseInt(num1) / parseInt(num2);
+    })
+
+
+
+
+    elemento = document.getElementById("num1");
+    elemento.addEventListener("click", function(){
+        elemento.value = "";
     })
 }
