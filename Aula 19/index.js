9ddb6ea702978e3b3fe2a386dec32a3dcefab6f6
Carlos <souzacef@gmail.com>
2024-07-12 09:49:08 -0300

    Update do projeto

--- Aula 19/index.js
+++ Aula 19/index.js
@@ -2,7 +2,7 @@ const prompt = require("prompt-sync")();
 
 let notas = [];
 
-function capturaNotas() {
+function capturarNotas() {
     const numNotas = +prompt("Quantas notas deseja informar? ");
 
 for (let i = 0; i < numNotas; i++) {
@@ -18,4 +18,31 @@ media = media / notas.length;
 return media;
 }
 
-console.log(capturaNotas());
+const media1 = capturarNotas();
+
+if(media1 >= 6) {
+    console.log("");
+    console.log(`Aluno aprovado com média ${media1}`);
+    console.log("Parabéns!");
+    console.log("");
+}else{
+    console.log("");
+    console.log(`Aluno não aprovado! Média: ${media1}`);
+    console.log("Encaminhado a recuperação!");
+    console.log("");
+
+    notas =[];
+    const media2 = capturarNotas();
+
+    if(media2 >= 6) {
+        console.log("");
+        console.log(`Aluno aprovado com média ${media2}`);
+        console.log("Parabéns!");
+        console.log("");
+    }else {
+        console.log("");
+        console.log(`Aluno não aprovado! Média: ${media2}`);        
+        console.log("");
+    }
+    
+}
